ccbc522b20c114c0539f86d84f313d0185a6d030
Nicholas Gower <nicholasgower01@gmail.com>
2019-11-28 12:44:34 -0800

    Added MathParser as a dependency

---
+++ TwoFunctionEquation.java
@@ -0,0 +1,21 @@
+import org.mariuszgromada.math.mxparser.*;
+ 
+/**
+ * Write a description of class TwoFunctionEquation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TwoFunctionEquation
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    Expression F=new Expression("1+2")
+    /**
+     * Constructor for objects of class TwoFunctionEquation
+     */
+    public TwoFunctionEquation()
+    {
+        
+    }
+}
